79272fcaae1dcb5bf0a8bc4b9f8fe284517d24b3
MrUnknown023 <90297051+MrUnknown023@users.noreply.github.com>
2022-09-01 00:01:49 -0600

    Add files via upload

--- js/editarPerfil.js
+++ js/editarPerfil.js
@@ -1,20 +1,28 @@
 function activateButton(){
     document.getElementById("confCambios").style.display='block';
+    document.getElementById("contrasenia").style.display='block';
     document.getElementById("confPasswordLabel").style.display='block';
-    document.getElementById("confPassword").style.display='block';
-    
 }
 
 function activateInputs(){
     document.getElementById("nombre").disabled=false;
     document.getElementById("apellido").disabled=false;
     document.getElementById("email").disabled=false;
-    document.getElementById("password").disabled=false;
-    document.getElementById("confPassword").disabled=false;
+    document.getElementById("cambiarContra").style.display='none';
     activateButton();
 }
 
 function fotoPerfil(){
     document.getElementById("subirArchivo").style.display='block'
     document.getElementById("confCambiosFoto").style.display='block';
+}
+
+function cambiarContrasenia(){
+    document.getElementById("cambiarContra").style.display='block';
+    document.getElementById("confCambios").style.display='none';
+    document.getElementById("contrasenia").style.display='none';
+    document.getElementById("confPasswordLabel").style.display='none';
+    document.getElementById("nombre").disabled=true;
+    document.getElementById("apellido").disabled=true;
+    document.getElementById("email").disabled=true;
 }
