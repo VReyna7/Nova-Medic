73b904e3550f14d0b2f7f2d34162b7bd1810ef78
MrUnknown023 <90297051+MrUnknown023@users.noreply.github.com>
2022-09-01 22:34:39 -0600

    Add files via upload

---
+++ js/registro.js
@@ -0,0 +1,36 @@
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const fecha = document.getElementById("fecha");
+const mensaje = document.getElementById("msgError");
+
+colorito.addEventListener("submit", e=>{
+    e.preventDefault();
+    let msgError = "";
+    let formatoEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let ingresar = false;
+    if(nombre.value.length < 6){
+        msgError += '-Nombre inválido <br>';
+        ingresar = true;
+    }
+    if(apellido.value.length < 6){
+        msgError += '-Apellido inválido <br>';
+        ingresar = true;
+    }
+    if(!formatoEmail.test(email.value)){
+        msgError += '-Correo electrónico inválido <br>';
+        ingresar = true;
+    }
+    if(password.value.length < 8){
+        msgError += '-Contraseña inválida <br>';
+        ingresar = true;
+    }
+    if(nombre.value.length == 0 && apellido.value.length == 0 && email.value.length == 0 && password.value.length == 0 && fecha.value.length == 0){
+        msgError = 'ERROR.DEBE LLENAR TODOS LOS CAMPOS';
+        ingresar = true;
+    }
+    if(ingresar){
+        mensaje.innerHTML = msgError;
+    }
+})
