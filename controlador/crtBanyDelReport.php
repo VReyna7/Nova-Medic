57ee1ce8a94eeb5808679603fff330ec1bafd7b8
Juan <89045210+VReyna7@users.noreply.github.com>
2022-09-06 22:32:27 -0600

    Add files via upload

--- controlador/crtBanyDelReport.php
+++ controlador/crtBanyDelReport.php
@@ -1,11 +1,31 @@
 <?php
 
-   $destino = "fm920715@gmail.com";
-   $nombre = "Francisco Mendoza" ;
-   $correo = "talentsystem2022@gmail.com";
-   $mensaje = "Buenas tardes";
-   $contenido = " Nombre : " .$nombre . " \ nCorreo : ". $correo ." \ nMensaje : " .$mensaje ;
-   mail ( $destino , " Contacto " , $contenido );
-   header("Location : index.php");
+require_once("../modelo/class.conexion.php");
+require_once("../modelo/class.reportes.php");
+require_once("../modelo/class.cliente.php");
+require_once("../modelo/class.doctor.php");
+
+$reportes = new Reportes;
+$doc = new Doctor;
+$client = new Cliente;
+
+
+$idReportado = isset($_GET['idReportado'])?$_GET['idReportado']:"";
+$idReportante = isset($_GET['idReportante'])?$_GET['idReportante']:"";
+$rol = isset($_GET['rol'])?$_GET['rol']:"";
+$accion = isset($_GET['accion'])?$_GET['accion']:"";
+
+if($accion=='Eliminar'){
+        $reportes->deleteReport($idReportante,$idReportado);
+        header("location: ../vistas/reporte.php");
+}else{
+        if($rol=="Doctor"){
+            $doc->Baneo($idReportado);
+            //header("location: ../vistas/reporte.php");
+        }else{
+            $client->Baneo($idReportado);
+           // header("location: ../vistas/reporte.php");
+        }
+}
 
 ?>
