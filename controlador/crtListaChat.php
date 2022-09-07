d29a6eb4f80de3ddb9dd3441c145369608151827
Cristian <79064096+Cr1ss7@users.noreply.github.com>
2022-09-06 23:29:51 -0600

    Add files via upload

--- controlador/crtListaChat.php
+++ controlador/crtListaChat.php
@@ -3,15 +3,20 @@
 require_once('../modelo/class.conexion.php');
 require_once('../modelo/class.chat.php');
 require_once('../modelo/class.consulta.php');
+require_once('../modelo/class.doctor.php');
 
 $chat = new Chat();
 $consult = new Consulta();
+$doc = new Doctor;
 
 $nameC = isset($_GET['nameC'])?$_GET['nameC']:"";
 $nameDC = isset($_GET['NameDC'])?$_GET['NameDC']:"";
 $idDC = isset($_GET['idDC'])?$_GET['idDC']:"";
 $idC = isset($_GET['idC'])?$_GET['idC']:"";
 $espec = isset($_GET['espe'])?$_GET['espe']:"";
+
+$doc->setDoctor($idDC);
+$doc->cambiarEstado(2);
 
 if($chat->searchChat($idC,$idDC)){
     header("location:../vistas/chat.php?");
