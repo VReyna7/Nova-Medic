25d64f8b0f22e590ae625fcbef8e3155e68b06da
Cristian <79064096+Cr1ss7@users.noreply.github.com>
2022-09-06 22:35:21 -0600

    Add files via upload

--- js/estado.js
+++ js/estado.js
@@ -1,9 +1,57 @@
+var selector = document.getElementById("selector");
+
+setInterval(()=>{
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "../controlador/crtGetEstado.php", true);
+	xhr.onload = ()=>{
+		if(xhr.readyState === XMLHttpRequest.DONE){
+			if(xhr.status === 200){
+				let data = xhr.response;
+				selector.value = data;
+			}
+		}
+	}
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send();
+
+	if(selector.value == "2"){
+		document.getElementById("disponible").style.display="none"
+		document.getElementById("ocupado").style.display="block"
+	}else if(selector.value == "1"){
+		document.getElementById("disponible").style.display="block"
+		document.getElementById("ocupado").style.display="none"
+	}
+},500)
+
 function disponible(){
-    document.getElementById("disponible").style.display="none"
-    document.getElementById("ocupado").style.display="block"
+    let xhr = new XMLHttpRequest();
+	xhr.open("POST", "../controlador/crtCambiarEstado.php", true);
+	xhr.onload = ()=>{
+		if(xhr.readyState == XMLHttpRequest.DONE){
+			if(xhr.status === 200){
+				document.getElementById("disponible").style.display="none"
+			    document.getElementById("ocupado").style.display="block"
+				selector.value = 2;
+			}
+		}
+	}
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("estado="+2);
 }
 
 function ocupado(){
-    document.getElementById("disponible").style.display="block"
-    document.getElementById("ocupado").style.display="none"
+    	let xhr = new XMLHttpRequest();
+	xhr.open("POST", "../controlador/crtCambiarEstado.php", true);
+	xhr.onload = ()=>{
+		if(xhr.readyState == XMLHttpRequest.DONE){
+			if(xhr.status === 200){
+				document.getElementById("disponible").style.display="block"
+			    document.getElementById("ocupado").style.display="none"
+				selector.value = 1;
+			}
+		}
+	}
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("estado="+1);
 }
+
