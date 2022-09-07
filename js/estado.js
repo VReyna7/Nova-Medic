14dd151bbbd6ca42511a6ed9db013530b0d8b08d
MrUnknown023 <90297051+MrUnknown023@users.noreply.github.com>
2022-09-06 21:03:47 -0600

    Add files via upload

---
+++ js/estado.js
@@ -0,0 +1,9 @@
+function disponible(){
+    document.getElementById("disponible").style.display="none"
+    document.getElementById("ocupado").style.display="block"
+}
+
+function ocupado(){
+    document.getElementById("disponible").style.display="block"
+    document.getElementById("ocupado").style.display="none"
+}
