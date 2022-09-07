7abd8768d21d895e57f91848afa8f3c49e2e2512
Cristian <79064096+Cr1ss7@users.noreply.github.com>
2022-09-07 17:56:22 -0600

    Add files via upload

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -78,13 +78,6 @@ if ($doctor) {
               <li class="nav-item">
                 <a class="nav-link fs-6 navbar-brand active" href="../vistas/perfil.php">PERFIL</a>
               </li>
-				<li class="nav-item">
-                <button class="nav-link fs-6 navbar-brand" id="disponible" onclick="disponible()">Disponible</button>
-                <button class="nav-link fs-6 navbar-brand" id="ocupado" onclick="ocupado()">Ocupado</button>
-				<input type="hidden" id="selector" value="">
-				
-              </li>
-
               <li class="nav-item">
                 <a class="nav-link fs-6 navbar-brand" href="../controlador/crtCerrarSesion.php">CERRAR SESION</a>
               </li>
