487fff89ee5b48ffc1512b38e287f2a6b81f5e13
Cristian <79064096+Cr1ss7@users.noreply.github.com>
2022-09-06 23:32:00 -0600

    Add files via upload

--- controlador/crtFinalizarConsult.php
+++ controlador/crtFinalizarConsult.php
@@ -2,13 +2,17 @@
 require_once('../modelo/class.conexion.php');
 require_once('../modelo/class.chat.php');
 require_once('../modelo/class.consulta.php');
+require_once('../modelo/class.doctor.php');
 
 $chat = new Chat();
 $consult = new Consulta();
+$doc = new Doctor;
 
 $idDC = isset($_GET['idDoc'])?$_GET['idDoc']:"";
 $idC = isset($_GET['dC'])?$_GET['dC']:"";
 
+$doc->setDoctor($idDC);
+$doc->cambiarEstado(1);
 
 echo $idC;
 echo $idDC;
